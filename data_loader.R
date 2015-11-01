a82dd5d3aaaa06b8b27d09b066dbf56c39d22950
keepcosmos <keepcosmos@gmail.com>
2015-11-01 15:54:55 +0900

    refactor data loader

--- data_loader.R
+++ data_loader.R
@@ -1,3 +1,40 @@
+library('data.table')
+library('dplyr')
+library('caret')
+source('data_loader.R')
+
+yelp.checkinByCategory <- function(category){
+  checkin <- yelp.readCheckin()
+  checkin[checkin$business_id %in% yelp.getBizIdsByCategory(category)]
+}
+
+yelp.bizAttrByCategory <- function(category){
+  bizAttr <- yelp.readBizAttr()
+  bizAttr[bizAttr$business_id %in% yelp.getBizIdsByCategory(category)]
+}
+
+yelp.getBizBaseByCategory <- function(category){
+  bizBase <- yelp.readBizBase()
+  bizBase[bizBase$business_id %in% yelp.getBizBaseByCategory(category)]
+}
+
+yelp.getBizIdsByCategory <- function(categ){
+  bizCat <- yelp.fetchBizCat()
+  bizCat[bizCat$category == categ]$business_id
+}
+
+yelp.fetchBizCat <- function(cache = TRUE){
+  if(cache && exists('fetchedBizCat')){
+    fetchedBizCat
+  }else{
+    fetchedBizCat <<- yelp.readBizCategory()
+    fetchedBizCat
+  }
+}
+
+
+#### - LOAD DATA - #####
+
 yelp.readUserBase <- function(){
   data.table(read.table('data/rda/user-base.Rda'))
 }
